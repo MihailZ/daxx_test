c401911baef57a1b39f2eed31620399544bf8024
mikhail.zlochevskyi <mikhail.zlochevskyi@bonial.com>
2017-06-01 23:00:08 +0300

    - describe Login, CreateProfile pages; - setup logger, driver init;

---
+++ src/test/java/com/daxx/pages/CreateProfilePage.java
@@ -0,0 +1,29 @@
+package com.daxx.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Created by mikhail_z on 6/1/17.
+ */
+public class CreateProfilePage {
+
+    public SelenideElement maleRadioBut = $("input[id=geslacht-m]");
+    public SelenideElement femaleRadioBut = $("input[id=geslacht-v]");
+
+    public SelenideElement firstNameInput = $("#voornaam");
+    public SelenideElement lastNameInput = $("#achternaam");
+
+    public SelenideElement birthDaySelect = $("#birthdayDay");
+    public SelenideElement birthMnthSelect = $("#selw-birthdayMonth");
+    public SelenideElement birthYearSelect = $("#selw-birthdayYear");
+
+    public SelenideElement emailInput = $("#email");
+    public SelenideElement phoneInput = $("#telefoon");
+    public SelenideElement gsmInput = $("#gsm");
+
+    public SelenideElement usernameInput = $("#username");
+    public SelenideElement pswInput = $("#psw");
+    public SelenideElement pswConfirmInput = $("#psw2");
+}
--- src/test/java/com/daxx/pages/LoginPage.java
+++ src/test/java/com/daxx/pages/LoginPage.java
@@ -1,9 +1,18 @@
 package com.daxx.pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 /**
  * Created by mikhail_z on 6/1/17.
  */
 public class LoginPage {
 
+    //better to use here @Getter from lombok plugin, to hide data
+    public SelenideElement emailInput = $("#login");
+    public SelenideElement passwordInput = $("#password");
+    public SelenideElement errorMsgLabel = $("div.errorMessage");
+    public SelenideElement submitBtn = $("#submit");
 
 }
